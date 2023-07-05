5dc072712ea5459a89436570e31794f47e798b76
André Storhaug <andr3.storhaug@gmail.com>
2023-07-06 00:19:09 +0200

    Update compile_readme.js

--- .github/scripts/compile_readme.js
+++ .github/scripts/compile_readme.js
@@ -1,57 +1,89 @@
+const core = require('@actions/core');
 const axios = require('axios');
 const fs = require('fs');
 
-async function getContents(url) {
-  try {
-    const response = await axios.get(url);
-    return response.data;
-  } catch (error) {
-    console.error(`Error fetching ${url}:`, error.message);
-    return '';
-  }
-}
-
-async function parseTableOfContents(contents) {
-  const regex = /^(\s*)\* \[(.*?)\]\((.*?)\)/gm;
+async function parseTableOfContents(url) {
+  const response = await axios.get(url);
+  const lines = response.data.split('\n');
+  const tocStartRegex = /^## Table of Contents$/;
+  const linkRegex = /^\s+\*\s+\[([^\]]+)\]\(([^)]+)\)(.*)$/;
   const toc = [];
-  let match;
 
-  while ((match = regex.exec(contents))) {
-    const indent = match[1].length;
-    const title = match[2];
-    const link = match[3];
-
-    toc.push({ indent, title, link });
+  let tocStarted = false;
+  for (const line of lines) {
+    if (!tocStarted) {
+      if (tocStartRegex.test(line)) {
+        tocStarted = true;
+      }
+    } else {
+      const linkMatch = line.match(linkRegex);
+      if (linkMatch) {
+        const title = linkMatch[1];
+        const link = linkMatch[2];
+        toc.push({ title, link });
+      }
+    }
   }
 
   return toc;
 }
 
-async function processLinks(toc) {
-  let readmeContents = '## Table of Contents\n\n';
-  let tocContents = '';
+async function isMarkdownFile(url) {
+  try {
+    const response = await axios.head(url);
+    const contentType = response.headers['content-type'];
+    return contentType.startsWith('text/plain');
+  } catch (error) {
+    return false;
+  }
+}
 
-  for (const { indent, title, link } of toc) {
-    const tocEntry = `${' '.repeat(indent)}* [${title}](${link})\n`;
+async function processLinks(baseURL, links, visited, output) {
+  for (const link of links) {
+    if (!visited.has(link.link)) {
+      visited.add(link.link);
+      const url = baseURL + link.link + '.md';
+      console.log(`Processing ${url}`);
+      const isMarkdown = await isMarkdownFile(url);
+      if (isMarkdown) {
+        const response = await axios.get(url);
+        const content = response.data;
 
-    tocContents += tocEntry;
+        // Remove relative links to the same file
+        const cleanedContent = content.replace(/\[([^\]]+)\]\(#\)/g, '');
 
-    if (!link.startsWith('http')) {
-      const fileContents = await getContents(link);
-      readmeContents += `\n# ${title}\n\n${fileContents.trim()}\n\n---\n`;
+        output.push(cleanedContent);
+
+        const nestedLinks = await parseTableOfContents(url);
+        await processLinks(baseURL, nestedLinks, visited, output);
+      } else {
+        console.log(`Skipping ${url} - Not a markdown file`);
+      }
     }
   }
-
-  return tocContents + '\n' + readmeContents;
 }
 
 async function run() {
-  const homePageURL = 'https://github.com/andstor/bibliography/wiki/Home.md';
-  const homePageContents = await getContents(homePageURL);
-  const toc = await parseTableOfContents(homePageContents);
-  const finalContents = await processLinks(toc);
+  try {
+    const baseURL = 'https://github.com/andstor/bibliography/wiki/';
+    const tocURL = baseURL + 'Home.md';
+    const toc = await parseTableOfContents(tocURL);
 
-  fs.writeFileSync('README.md', finalContents);
+    const visited = new Set();
+    const output = [];
+
+    await processLinks(baseURL, toc, visited, output);
+
+    // Combine all contents into a single README file
+    const readmeContent = tocURL + '\n\n' + output.join('\n\n');
+
+    // Write the README file
+    fs.writeFileSync('README.md', readmeContent);
+
+    console.log('README file created successfully!');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
-run().catch(console.error);
+run();
